fe6e111920f2617a353e4304b4fb40d10bbcbbda
Aldo Pelayo <aldoorronaldo@gmail.com>
2022-05-23 04:09:11 -0400

    timer added

--- Assets/script.js
+++ Assets/script.js
@@ -1,33 +1,46 @@
 //variables
-const StartBtn = document.querySelector('#StartQuiz');
+var StartBtn = document.querySelector('#StartQuiz');
 const FirstPanel = document.querySelector('#OpeningMessage');
 
-var a=0;
-a++;
-var b=0;
-b++;
-var c=60;
+var secs =60
 
 //lets make the start button commence things
+
 StartBtn.addEventListener('click', () =>{
+    
     if(FirstPanel.style.display === 'none') {
         FirstPanel.style.display = 'block';
     } else{
         FirstPanel.style.display='none';
      }
+     
 });
 
+
 //lets make a timer
-function Timer1() {
-    c= c - 1;
-    if (c<60) {
-        timerText.innerHTML = c;
+function timer() {
+    var element =document.getElementById("timerText");
+    element.innerHTML="time left"+" "+secs--;
+    setTimeout(timer, 1000);
+    if (secs<1){
+        secs=60;
+        element.innerHTML='<h2>Times up!</h2>';
     }
-    if(C<1){
-        Window.ClearInterval(update);
-    }
+    
 }
-    update=setInterval("Timer1()",1000);
+//     StartBtn.addEventListener('click',()=>{
+    
+//     c= c - 1;
+//     if (c<60) {
+//         timerText.innerHTML = c;
+//     }
+//     if(C<1){
+//         Window.ClearInterval(update);
+//     }
+//     }
+// )
+//     c=60;
+//     update=setInterval("Timer1()",1000);
 
 //lets go to our new question
 function NextQuestion(){
