7cd31f3a50aef60b0e477b16f465a99dfec69813
Aldo Pelayo <aldoorronaldo@gmail.com>
2022-05-23 05:11:05 -0400

    added pages

--- Assets/script.js
+++ Assets/script.js
@@ -43,9 +43,32 @@ function timer() {
 //     update=setInterval("Timer1()",1000);
 
 //lets go to our new question
-function NextQuestion(){
+        $("#question1").click(function(){
+            $("#question1").hide();
+            $("#question2").show();
+});
+$("#question2").click(function(){
+    $("#question2").hide();
+    $("#question3").show();
+});
+$("#question3").click(function(){
+    $("#question3").hide();
+    $("#question4").show();
+});
+$("#question4").click(function(){
+    $("#question4").hide();
+    $("#question5").show();
+});
+$("#question5").click(function(){
+    $("#question5").hide();
+    $("#question6").show();
+});
+$("#question6").click(function(){
+    $("#question6").hide();
+});
+//next we can show the submit score card !
 
-}
+
 //logic to our right answers and wrong answers
 function AnswerTracker(){
 
